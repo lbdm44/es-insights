af0f38567f77f78962ed5945558bbb08e94ceacb
Lewis Miller <lemiller@lemiller-mn1.linkedin.biz>
2022-03-10 09:59:49 -0800

    adding first package

---
+++ packages/export-analyzer/dist/index.js
@@ -0,0 +1,15 @@
+function $parcel$defineInteropFlag(a) {
+  Object.defineProperty(a, '__esModule', {value: true, configurable: true});
+}
+function $parcel$export(e, n, v, s) {
+  Object.defineProperty(e, n, {get: v, set: s, enumerable: true, configurable: true});
+}
+
+$parcel$defineInteropFlag(module.exports);
+
+$parcel$export(module.exports, "default", () => $4da48ec6a18dc112$export$2e2bcd8739ae039);
+var $4da48ec6a18dc112$export$2e2bcd8739ae039 = ()=>'sdsdsd'
+;
+
+
+//# sourceMappingURL=index.js.map
---
+++ packages/export-analyzer/dist/module.js
@@ -0,0 +1,6 @@
+var $12c4890f67a0eb85$export$2e2bcd8739ae039 = ()=>'sdsdsd'
+;
+
+
+export {$12c4890f67a0eb85$export$2e2bcd8739ae039 as default};
+//# sourceMappingURL=module.js.map
